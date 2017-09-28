43a38606234740930d1e8565d4a9c647405e4e8e
Aruna Chinnathambi <aruna.c@KGFSL.COM>
2017-09-28 10:30:56 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
-import { CounterService } from './counter/counter.service';
+//import { CounterService } from './counter/counter.service';
 import { HttpModule } from '@angular/http';
 @NgModule({
   declarations: [
